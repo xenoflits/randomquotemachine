0c6d5ad663027d2c5e8f160cdde85cd027a669a8
xenoflits <soeradj.mahabiersing@gmail.com>
2020-05-02 11:41:36 +0200

    some more bug fixes

--- src/components/quotefield.js
+++ src/components/quotefield.js
@@ -4,7 +4,11 @@ const QuoteField = (props) => {
 
     let quote = props.quote.replace(/<p[^>]*>|<\/p>/g,"")
     .replace(/&#8217;/g,"'")
-    .replace(/&#8211;/g,"-");
+    .replace(/&#8211;/g,"-")
+    .replace(/&#038;/g,"&")
+    .replace(/&#8216;/g,'`')
+    .replace(/&#8220;/,"“")
+    .replace(/&#8221;/,"”");
     
     
    return (<div className="quotefield">
