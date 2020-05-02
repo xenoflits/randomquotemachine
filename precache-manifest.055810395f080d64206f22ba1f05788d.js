7e80f273b6f0fb3a8da0d84c7b13c988ef40fef5
xenoflits <soeradj.mahabiersing@gmail.com>
2020-05-02 11:35:15 +0200

    Updates

---
+++ precache-manifest.055810395f080d64206f22ba1f05788d.js
@@ -0,0 +1,26 @@
+self.__precacheManifest = (self.__precacheManifest || []).concat([
+  {
+    "revision": "3658c94356e0b2941b21cd865ecce361",
+    "url": "/randomquotemachine/index.html"
+  },
+  {
+    "revision": "53899f1e0687c2ee13ab",
+    "url": "/randomquotemachine/static/css/main.f2f55068.chunk.css"
+  },
+  {
+    "revision": "3e44ed222716a068bf63",
+    "url": "/randomquotemachine/static/js/2.c74c1149.chunk.js"
+  },
+  {
+    "revision": "e88a3e95b5364d46e95b35ae8c0dc27d",
+    "url": "/randomquotemachine/static/js/2.c74c1149.chunk.js.LICENSE.txt"
+  },
+  {
+    "revision": "53899f1e0687c2ee13ab",
+    "url": "/randomquotemachine/static/js/main.3c4b1a9e.chunk.js"
+  },
+  {
+    "revision": "03dae0311008057c4725",
+    "url": "/randomquotemachine/static/js/runtime-main.9816759e.js"
+  }
+]);
