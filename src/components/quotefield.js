f4e50d7badea21d1c94a1f9b88e5393ebe32729c
xenoflits <soeradj.mahabiersing@gmail.com>
2020-05-02 11:23:52 +0200

    2nd day adjustments on css, responsiveness

--- src/components/quotefield.js
+++ src/components/quotefield.js
@@ -2,8 +2,11 @@ import React from 'react'
 
 const QuoteField = (props) => {
 
-    let quote = props.quote.replace(/<p[^>]*>|<\/p>/g,"");
-    quote = quote.replace(/&#8217;/g,"'"); 
+    let quote = props.quote.replace(/<p[^>]*>|<\/p>/g,"")
+    .replace(/&#8217;/g,"'")
+    .replace(/&#8211;/g,"-");
+    
+    
    return (<div className="quotefield">
        <div className="quotefield-quote">{quote}</div>
        <div className="quotefield-title">{props.title}</div>
